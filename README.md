c4644dce8e639e94fdde367a1b6a42e14dec5e71
Andreas <asc4asc@users.noreply.github.com>
2020-05-13 16:23:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Ein weiters einfaches Beispiel für die TRP und Kampfrichter Ausbildung!
+
+Ein einzelnes Gefecht DA auf z.B auf 15 Punkte!
+
+Hier jetzt erstmal so das man es gut testen kann.
+
+Programm technisch ganz einfach gehalten klein und ohne Schleifen .... Daten sind direkt in der Webseite.
+
+Wobei die Namen u.s.w. noch sprechend aufbereitet werden sollten.
+
+Javascript code muss noch bereinigt werden. 
+
+Button ohne Sinn auf inaktiv wäre nicht schlecht! 
+
+Farben Rot noch passend ändern? Wäre gut wenn auch Bootstrap Themes berücksichtig würden.
+
+PC = Passivität Karte. Gelb --> 2 * Rot --> Schwarz (Sieg für den Gegner wenn dieser nicht auch Schwarz hat.) Rangliste entscheidet dann ist aber hier nicht verfügbar.
+
+Untere Uhr für 1min Pause und Sudden Death!
+
+C1 soll nicht das Ergebniss löschen und somit Korrekturen ermöglichen. Scheint nicht immer sinnvoll nutzbar.
---
+++ stopWatch.js
@@ -0,0 +1,246 @@
+// To mutch global variables!
+var startTimerButton = document.querySelector('.startTimer');
+var startTimerButton1 = document.querySelector('.startTimer1');
+var startTimerButton2 = document.querySelector('.startTimer2');
+var pauseTimerButton = document.querySelector('.pauseTimer');
+var pauseTimerButton1 = document.querySelector('.pauseTimer1');
+var pauseTimerButton2 = document.querySelector('.pauseTimer2');
+var timerDisplay = document.querySelector('.timer');
+var timerDisplay1 = document.querySelector('.timer1');
+var timerDisplay2 = document.querySelector('.timer2');
+
+var startTime;
+var startTime1;
+var startTime2;
+var updatedTime;
+var updatedTime1;
+var updatedTime2;
+var difference;
+var difference1;
+var difference2;
+var tInterval;
+var tInterval1;
+var tInterval2;
+var savedTime;
+var savedTime1;
+var savedTime2;
+var paused = 0;
+var paused1 = 0;
+var paused2 = 0;
+var running = 0;
+var running1 = 0;
+var running2 = 0;
+
+
+// functions can merged together!!
+function startTimer(){
+  if(!running){
+    startTime = new Date().getTime();
+    tInterval = setInterval(getShowTime, 100);
+    paused = 0;
+    running = 1;
+    timerDisplay.style.cursor = "auto";
+    timerDisplay.style.color = "yellow";
+    startTimerButton.classList.add('lighter');
+    pauseTimerButton.classList.remove('lighter');
+    startTimerButton.style.cursor = "auto";
+    pauseTimerButton.style.cursor = "pointer";
+  }
+}
+
+function startTimer1(){
+  if(!running1){
+    startTime1 = new Date().getTime();
+    tInterval1 = setInterval(getShowTime1, 100);
+    paused1 = 0;
+    running1 = 1;
+    timerDisplay1.style.cursor = "auto";
+    timerDisplay1.style.color = "yellow";
+    startTimerButton1.classList.add('lighter');
+    pauseTimerButton1.classList.remove('lighter');
+    startTimerButton1.style.cursor = "auto";
+    pauseTimerButton1.style.cursor = "pointer";
+  }
+}
+
+function startTimer2(){
+  if(!running2){
+    startTime2 = new Date().getTime();
+    tInterval2 = setInterval(getShowTime2, 100);
+    paused2 = 0;
+    running2 = 1;
+    timerDisplay2.style.cursor = "auto";
+    timerDisplay2.style.color = "yellow";
+    startTimerButton2.classList.add('lighter');
+    pauseTimerButton2.classList.remove('lighter');
+    startTimerButton2.style.cursor = "auto";
+    pauseTimerButton2.style.cursor = "pointer";
+  }
+}
+
+function pauseTimer(){
+  if (!difference){
+    // if timer never started, don't allow pause button to do anything
+  } else if (!paused) {
+    clearInterval(tInterval);
+    savedTime = difference;
+    paused = 1;
+    running = 0;
+    timerDisplay.style.cursor = "pointer";
+    startTimerButton.classList.remove('lighter');
+    pauseTimerButton.classList.add('lighter');
+    startTimerButton.style.cursor = "pointer";
+    pauseTimerButton.style.cursor = "auto";
+  } else {
+    startTimer();
+  }
+}
+
+function pauseTimer1(){
+  if (!difference1){
+    // if timer never started, don't allow pause button to do anything
+  } else if (!paused1) {
+    clearInterval(tInterval1);
+    savedTime1 = difference1;
+    paused1 = 1;
+    running1 = 0;
+    timerDisplay1.style.cursor = "pointer";
+    startTimerButton1.classList.remove('lighter');
+    pauseTimerButton1.classList.add('lighter');
+    startTimerButton1.style.cursor = "pointer";
+    pauseTimerButton1.style.cursor = "auto";
+  } else {
+    startTimer1();
+  }
+}
+
+function pauseTimer2(){
+  if (!difference2){
+    // if timer never started, don't allow pause button to do anything
+  } else if (!paused2) {
+    clearInterval(tInterval2);
+    savedTime2 = difference2;
+    paused2 = 1;
+    running2 = 0;
+    timerDisplay2.style.cursor = "pointer";
+    startTimerButton2.classList.remove('lighter');
+    pauseTimerButton2.classList.add('lighter');
+    startTimerButton2.style.cursor = "pointer";
+    pauseTimerButton2.style.cursor = "auto";
+  } else {
+    startTimer2();
+  }
+}
+
+
+
+function resetTimer(){
+  clearInterval(tInterval);
+  savedTime = 0;
+  difference = 0;
+  paused = 0;
+  running = 0;
+  timerDisplay.innerHTML = '3:00:000';
+  timerDisplay.style.cursor = "pointer";
+  startTimerButton.classList.remove('lighter');
+  pauseTimerButton.classList.remove('lighter');
+  startTimerButton.style.cursor = "pointer";
+  pauseTimerButton.style.cursor = "auto";
+}
+
+function resetTimer1(){
+  clearInterval(tInterval1);
+  savedTime1 = 0;
+  difference1 = 0;
+  paused1 = 0;
+  running1 = 0;
+  timerDisplay1.innerHTML = '1:00';
+  timerDisplay1.style.cursor = "pointer";
+  startTimerButton1.classList.remove('lighter');
+  pauseTimerButton1.classList.remove('lighter');
+  startTimerButton1.style.cursor = "pointer";
+  pauseTimerButton1.style.cursor = "auto";
+}
+
+function resetTimer2(){
+  clearInterval(tInterval2);
+  savedTime2 = 0;
+  difference2 = 0;
+  paused2 = 0;
+  running2 = 0;
+  timerDisplay2.innerHTML = '1:00:000';
+  timerDisplay2.style.cursor = "pointer";
+  startTimerButton2.classList.remove('lighter');
+  pauseTimerButton2.classList.remove('lighter');
+  startTimerButton2.style.cursor = "pointer";
+  pauseTimerButton2.style.cursor = "auto";
+}
+
+function getShowTime(){
+  updatedTime = new Date().getTime();
+  if (savedTime){
+    difference = (updatedTime - startTime) + savedTime;
+  } else {
+    difference =  updatedTime - startTime;
+  }
+  var e = new Date(difference);
+  var minutes = e.getMinutes();
+  minutes = (minutes < 10) ?  minutes : minutes;
+  
+  if (minutes >2 ) { pauseTimer(); pauseTimer1(); };
+  minutes = 2-1*minutes; 
+  minutes = (minutes < 10) ?  minutes : minutes;
+
+  var seconds = e.getSeconds();
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+  seconds = 59-1*seconds;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+  
+  var milliseconds = e.getMilliseconds();
+  milliseconds = (milliseconds < 100) ? (milliseconds < 10) ? "00" + milliseconds : "0" + milliseconds : milliseconds;
+  timerDisplay.innerHTML = minutes + ':' + seconds + ':' + milliseconds;
+}
+
+
+function getShowTime1(){
+  updatedTime1 = new Date().getTime();
+  if (savedTime1){
+    difference1 = (updatedTime1 - startTime1) + savedTime1;
+  } else {
+    difference1 =  updatedTime1 - startTime1;
+  }
+  var e = new Date(difference1);
+  var minutes1 = e.getMinutes();
+  minutes1 = (minutes1 < 10) ?  minutes1 : minutes1;
+  var seconds1 = e.getSeconds();
+  seconds1 = (seconds1 < 10) ? "0" + seconds1 : seconds1;
+  
+  if (minutes1 > 0) { resetTimer1(); pauseTimer1(); };
+  minutes1 = 0;
+  seconds1 = 59-1*seconds1;
+  seconds1 = (seconds1 < 10) ? "0" + seconds1 : seconds1;
+  timerDisplay1.innerHTML = minutes1 + ':' + seconds1;
+}
+
+function getShowTime2(){
+  updatedTime2 = new Date().getTime();
+  if (savedTime2){
+    difference2 = (updatedTime2 - startTime2) + savedTime2;
+  } else {
+    difference2 =  updatedTime2 - startTime2;
+  }
+  var e = new Date(difference2);
+  var minutes2 = e.getMinutes();
+  minutes2 = (minutes2 < 10) ?  minutes2 : minutes2;
+  var seconds2 = e.getSeconds();
+  seconds2 = (seconds2 < 10) ? "0" + seconds2 : seconds2;
+    
+  if (minutes2 > 0) { pauseTimer2(); };
+  minutes2 = 0;
+  seconds2 = 59-1*seconds2;
+  seconds2 = (seconds2 < 10) ? "0" + seconds2 : seconds2;
+  
+  var milliseconds2 = e.getMilliseconds();
+  milliseconds2 = (milliseconds2 < 100) ? (milliseconds2 < 10) ? "00" + milliseconds2 : "0" + milliseconds2 : milliseconds2;
+  timerDisplay2.innerHTML = minutes2 + ':' + seconds2 + ':' + milliseconds2;
+}
